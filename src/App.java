004203ac423a72388cb1ff6f6a6d53737a44a379
Sangeeta Bose <sangeeta.bose.236@gmail.com>
2025-07-29 22:06:51 +0530

    Solution

---
+++ src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println(shouldWakeUp(true, 1));
+        System.out.println(shouldWakeUp(false, 2));
+        System.out.println(shouldWakeUp(true, 8));
+        System.out.println(shouldWakeUp(true, -1));
+
+    }
+
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+        if (hourOfDay < 0 || hourOfDay > 23) {
+            return false;
+        }
+
+        return barking && (hourOfDay < 8 || hourOfDay > 22);
+    }
+}
